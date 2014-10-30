5e413cd1b073eb4c4db564d49f3e0b7e0b982111
Mark <github880520@mstier.de>
2014-10-30 17:53:06 +0100

    apt cache

--- create_img.sh
+++ create_img.sh
@@ -6,14 +6,14 @@ set -o pipefail
 distname=precise
 tgtdir="$(pwd)/$distname"
 
-apt-get update
-apt-get install debootstrap
-debootstrap \
+sudo apt-get update
+sudo apt-get install debootstrap
+sudo debootstrap \
     --arch=amd64 \
     --variant=minbase \
     --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg \
-    $distname $tgtdir \
-    http://de.archive.ubuntu.com/ubuntu/
+    "$distname" "$tgtdir" \
+    http://archive.ubuntu.com/ubuntu/
 
-find $tgtdir
+sudo find $tgtdir
 
